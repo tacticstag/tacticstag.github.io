919b51ce849b09acb97aa38907db15bc90f2614a
tacticstag <whiskerwonderstv@gmail.com>
2024-09-12 10:16:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Function to toggle button visibility
-function toggleButtonVisibility(buttonId) {
-    const button = document.getElementById(buttonId);
-
-    // Toggle the 'hidden' class
-    if (button.classList.contains('hidden')) {
-        button.classList.remove('hidden');
-    } else {
-        button.classList.add('hidden');
-    }
-}
-
 // Function to open a website when a button is clicked
 function openWebsite(url) {
     window.open(url, '_blank');  // Opens the website in a new tab
