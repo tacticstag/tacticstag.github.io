2a0a228a98c4211338cf31d49e2839db2f44522a
tacticstag <whiskerwonderstv@gmail.com>
2024-09-12 10:10:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function showMessage() {
-    document.getElementById('message').innerText = "Hello, this is a message from JavaScript!";
+// Function to toggle button visibility
+function toggleButtonVisibility(buttonId) {
+    const button = document.getElementById(buttonId);
+
+    // Toggle the 'hidden' class
+    if (button.classList.contains('hidden')) {
+        button.classList.remove('hidden');
+    } else {
+        button.classList.add('hidden');
+    }
+}
+
+// Function to open a website when a button is clicked
+function openWebsite(url) {
+    window.open(url, '_blank');  // Opens the website in a new tab
 }
